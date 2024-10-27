74bb03ad4560792386cd79c2fe97202aba7ed573
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-10-27 15:33:59 +0500

    Add client model

--- src/main/java/com/ok/validation/controller/ClientController.java
+++ src/main/java/com/ok/validation/controller/ClientController.java
@@ -1,4 +1,33 @@
 package com.ok.validation.controller;
 
+import com.ok.validation.model.Client;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ClientController {
+
+	@GetMapping("/client")
+	public String showClientForm(Model model) {
+
+		model.addAttribute("client", new Client());
+		return "client-form";
+	}
+
+	@PostMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("client") Client client,
+	                          BindingResult bindingResult, Model model) {
+
+
+		if (bindingResult.hasErrors()) {
+			return "client-form";
+		} else {
+			return "client-confirmation";
+		}
+	}
 }
--- src/main/java/com/ok/validation/model/Client.java
+++ src/main/java/com/ok/validation/model/Client.java
@@ -1,4 +1,15 @@
 package com.ok.validation.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class Client {
+
+	private String firstName;
+
+	@NotNull(message = "Enter last name.")
+	@Size(min = 2, max = 100, message = "Enter correct last name")
+	private String lastName;
 }
