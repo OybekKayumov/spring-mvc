0d5adc7f9f7a177c55ca07f64788325a0ca62ddd
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-11-03 10:59:15 +0500

    Add stringTrimmerEditor

--- src/main/java/com/ok/validation/controller/ClientController.java
+++ src/main/java/com/ok/validation/controller/ClientController.java
@@ -2,15 +2,24 @@ package com.ok.validation.controller;
 
 import com.ok.validation.model.Client;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClientController {
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		binder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
 	@GetMapping("/client")
 	public String showClientForm(Model model) {
