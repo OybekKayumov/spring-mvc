e7b134bf8f1ed59688c3b2fe78d1fadee050ef1d
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-11-03 11:11:33 +0500

    Add age validations

--- src/main/java/com/ok/validation/model/Client.java
+++ src/main/java/com/ok/validation/model/Client.java
@@ -1,5 +1,7 @@
 package com.ok.validation.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -13,4 +15,9 @@ public class Client {
 	@Size(min = 2, max = 100, message = "Enter correct last name, min 2 " +
 					"symbols, max 100 symbols")
 	private String lastName;
+
+	@Min(value = 18, message = "Client must be older than 17")
+	@Max(value = 125, message = "Client must be younger than 126")
+	private int age;
+
 }
