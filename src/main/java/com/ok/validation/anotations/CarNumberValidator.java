8e0b0d2d6e288d4e4beb529fd0c05b24bccb8161
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-11-10 05:57:23 +0500

    Add if null check

--- src/main/java/com/ok/validation/anotations/CarNumberValidator.java
+++ src/main/java/com/ok/validation/anotations/CarNumberValidator.java
@@ -17,6 +17,9 @@ public class CarNumberValidator implements ConstraintValidator<CarNumber, String
 	public boolean isValid(String carNumber,
 	                       ConstraintValidatorContext constraintValidatorContext) {
 
+		if (carNumber == null || countryCode == null) {
+			return false;
+		}
 		carNumber = carNumber.toUpperCase();
 
 		boolean result = carNumber.endsWith(countryCode);
